66333c67e22f37b7fd95f9a1b2f876136314991c
Omega <omega@alpha.org>
2015-06-16 21:47:56 -0400

    8000-1000bits/2min

--- main.py
+++ main.py
@@ -8,16 +8,20 @@ import TRNG
 import time
 if __name__ == "__main__":
   while True:
-    print "Generating random string.." #State what is going to happen
-    rng = TRNG.RNG() #Start up a TRNG instance
-    rng.generate() #Generate some fresh data
-    rng.xor() #Xor the data
-    rng.randomize() #Multiply by time stamp in micro seconds
-    rng.xor() #Xor the data again
-    rng.hex() #Convert to hex
-    rng.hexToChars() #Convert printable hex values to chars
-    rng.shift() #Split data in half and combine data while shifting pos by 1
-    rng.append() #Append to bits.txt
-    print rng.getSelf() #Show the resulting string
-    print "Sleeping for 120 seconds..." #State we are ready to sleep
-    time.sleep(120) #Sleep for two minutes for more entropy
+    try:
+        print "Generating random string.." #State what is going to happen
+        rng = TRNG.RNG() #Start up a TRNG instance
+        rng.generate() #Generate some fresh data
+        rng.xor() #Xor the data
+        rng.randomize() #Multiply by time stamp in micro seconds
+        rng.xor() #Xor the data again
+        rng.hex() #Convert to hex
+        rng.hexToChars() #Convert printable hex values to chars
+        rng.shift() #Split data in half and combine data while shifting pos by 1
+        rng.append() #Append to bits.txt
+        print rng.getSelf() #Show the resulting string
+        print "Sleeping for 120 seconds..." #State we are ready to sleep
+        time.sleep(120) #Sleep for two minutes for more entropy
+    except Exception as e:
+        print "Error: " + str(e)
+        print "Something went wrong. Retrying.."
